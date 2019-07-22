d26116780341a1014dc54d9e3d50cfce43ce6ce2
oadziel <oadziel>
2019-07-22 22:11:46 +0100

    Final

---
+++ AR/AR_Unity_Project/Assets/Scripts/CS_VB_EventHandler.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class CS_VB_EventHandler : MonoBehaviour
+{
+    [SerializeField] private GameObject[] VLights;
+    [SerializeField] private AudioSource aud;
+    [SerializeField] private AudioClip clip;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    public void OnLightPressed()
+    {
+        Debug.Log("VB Pressed");
+        foreach(GameObject Vlight in VLights)
+        {
+            if (Vlight.activeSelf == true)
+            {
+                Vlight.SetActive(false);
+            }
+            else
+            {
+                Vlight.SetActive(true);
+            }
+        }
+    }
+
+    public void OnHornPressed()
+    {
+        aud.PlayOneShot(clip, 1);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
